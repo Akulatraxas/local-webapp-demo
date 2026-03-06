8f97ceb7def8f1bdeff7de065d7f45e8ca1a11a4
Akulatraxas <103323+Akulatraxas@users.noreply.github.com>
2026-03-06 01:57:15 +0100

    iforgot a comma

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,6 @@ app = FastAPI()
 def root():
     return {
         "message": "Hello from backend",
-        "environment": os.getenv("APP_ENV", "not-set")
-        "apikey": os.getenv("API_KEY", "not-set")
+        "environment": os.getenv("APP_ENV", "not-set"),
+        "apikey": os.getenv("API_KEY", "not-set"),
     }
