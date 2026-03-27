6e4345bdfca46c24df13fe507a76a8147a4def7c
Akulatraxas <103323+Akulatraxas@users.noreply.github.com>
2026-03-28 00:25:33 +0100

    added secret loaded from infisica

--- backend/app.py
+++ backend/app.py
@@ -10,4 +10,5 @@ def root():
         "environment": os.getenv("APP_ENV", "not-set"),
         "apikey": os.getenv("API_KEY", "not-set"),
         "akudesires": os.getenv("AKUDESIRES", "not-set"),
+        "aku_from_infisical": os.getenv("AKU_FROM_INFISICAL", "not-set"),
     }
